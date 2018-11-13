846096d28d4bca45da172a58538ca0efdaaf4068
dakoo <dakoo@users.noreply.github.com>
2018-11-12 21:58:27 -0800

    Implemented the tag to alias and executeCommand

--- quick-directory-aliases.sh
+++ quick-directory-aliases.sh
@@ -24,18 +24,19 @@ touch "$_d_mapFile"
 
 _d_usage()
 {
-    printf "usage: d [+|-] [alias]\n" >&2
+    printf "usage: d [+|-|e|c all] [alias] [tag|command]\n" >&2
     printf "\n" >&2
-    printf "Add:      d + aliasName\n" >&2
+    printf "Add:      d + aliasName \"tag\"\n" >&2
     printf "Remove:   d - aliasName\n" >&2
+    printf "ExcuteCommand: d e aliasName command \n" >&2
     printf "Navigate: d aliasName\n" >&2
     printf "List all: d\n" >&2
+    printf "Clear: d c all\n" >&2
+    printf "Help: d ?\n" >&2
     printf "\n" >&2
     printf "Version: 1.2\n" >&2
     printf "\n" >&2
     printf "Full README: https://github.com/mcwoodle/shell-directory-management/blob/master/README.md\n" >&2
-
-    _d_setupAutoComplete
 }
 
 # ensure there isn't an alias set with the same name.
@@ -51,6 +52,9 @@ d()
     _d_curDir=`pwd`
     _d_addDir=false
     _d_removeDir=false
+    _d_excuteCommand=false
+    _d_clear=false
+    _d_help=false
     _d_aliasName=
     _d_finished=false # an awkward workaround for not calling exit
 
@@ -58,9 +62,8 @@ d()
     if [ "$#" -eq "0" ]
     then
         cat $_d_mapFile
-        _d_setupAutoComplete
         return 0
-    elif [ "$#" -gt "2" ]
+    elif [ "$#" -gt "3" ]
     then
         _d_usage
         return 1
@@ -72,6 +75,9 @@ d()
         case "$1" in
             +) _d_addDir=true;;
             -) _d_removeDir=true;;
+            e) _d_excuteCommand=true;;
+            c) _d_clear=true;;
+            ?) _d_help=true;;
             -h) _d_usage; return 0;;
             -*) _d_usage; return 1;;
             *) _d_aliasName=$1; break;;	# terminate while loop
@@ -91,15 +97,38 @@ d()
     then
         if [ -z "$_d_aliasRow" ]
         then
-            #Write the new alias to our map file
-            printf "$_d_aliasName = $_d_curDir\n" >> $_d_mapFile
-            retVal=$?
-            _d_setupAutoComplete
-            return $retVal
+	    #Write the new alias to our map file
+            if [ "$#" -eq "2" ]
+	    then
+                printf "$_d_aliasName = $_d_curDir -> $2\n" >> $_d_mapFile
+                return $? 
+	    else
+                printf "$_d_aliasName = $_d_curDir -> .\n" >> $_d_mapFile
+                return $? 
+	    fi
         else
             printf "The map alias $_d_aliasName already exists:\n$_d_aliasRow\n"
             return 1
         fi
+    fi
+
+    if $_d_clear;
+    then
+        if [ "$1" = "all" ] 
+        then
+            rm "$_d_mapFile" && touch "$_d_mapFile"
+            printf "Cleared the directory information\n"
+            return 1
+        else
+            _d_usage
+            return 1
+        fi
+    fi
+
+    if $_d_help;
+    then
+        _d_usage
+        return 1
     fi
 
     if [ -z "$_d_aliasRow" ]
@@ -114,19 +143,30 @@ d()
         if [ "$?" -eq "0" ]
         then
             printf "$_d_aliasName successfully removed\n"
-            retVal=$?
-            _d_setupAutoComplete
-            return $retVal
+            return $?
         else
             printf "Error removing $_d_aliasName\n"
             return 1
         fi
     fi
 
+    if $_d_excuteCommand;
+    then
+        _d_cmd=`printf "$_d_aliasRow" | sed -e "s,.* = \(.*\) -> .*,\1,"`
+        printf "cd $_d_cmd\n"
+        case "$2" in
+            ij) /usr/local/bin/idea $_d_cmd;;
+            st) stree $_d_cmd;;
+            op) open $_d_cmd;;          
+            *) "$2" $_d_cmd;;
+        esac
+        return 1
+    fi
+
     # Actually change the directory.
-    _d_cmd=`printf "$_d_aliasRow" | sed -e "s,.* = \(.*\),\1,"`
+    _d_cmd=`printf "$_d_aliasRow" | sed -e "s,.* = \(.*\) -> .*,\1,"`
     printf "cd $_d_cmd\n"
-    cd "$_d_cmd"
+    cd $_d_cmd
     return $?
 }
 
@@ -138,36 +178,29 @@ d()
 # Inspiration for the zsh/bash autocomplete impl from:
 # http://jeroenjanssens.com/2013/08/16/quickly-navigate-your-filesystem-from-the-command-line.html
 
-_d_setupAutoComplete_zsh()
-{
-    # Note: Use eval to avoid breaking 'sh' posix or shells
-    eval "reply=($(sed -e 's/\(.*\) = .*/\1/' $_d_mapFile))"
-    return 0
-}
+# For zsh, test if the compctl command exists.
+if type compctl >/dev/null 2>&1
+then
+    _d_setupAutoComplete_zsh()
+    {
+        # Note: Use eval to avoid breaking 'sh' posix or shells
+        eval "reply=($(sed -e 's/\(.*\) = .*/\1/' $_d_mapFile))"
+        return 0
+    }
+    compctl -K _d_setupAutoComplete_zsh d >/dev/null 2>&1
+fi
 
-_d_setupAutoComplete_bash()
-{
-    local curw=${COMP_WORDS[COMP_CWORD]}
-    local wordlist=$(sed -e "s/\(^.*\) = .*/\1/" $_d_mapFile)
-    # Note: Use eval to avoid breaking 'sh' posix or shells
-    eval "COMPREPLY=($(compgen -W "$wordlist" -- "$curw"))"
-    return 0
-}
+# For bash, test if the compgen/complete commands exists
+if type compgen >/dev/null 2>&1
+then
+    _d_setupAutoComplete_bash()
+    {
+        local curw=${COMP_WORDS[COMP_CWORD]}
+        local wordlist=$(sed -e "s/\(^.*\) = .*/\1/" $_d_mapFile)
+        # Note: Use eval to avoid breaking 'sh' posix or shells
+        eval "COMPREPLY=($(compgen -W "$wordlist" -- "$curw"))"
+        return 0
+    }
 
-_d_setupAutoComplete()
-{
-
-    # For zsh, test if the compctl command exists.
-    if type compctl >/dev/null 2>&1
-    then
-        _d_setupAutoComplete_zsh
-        compctl -K _d_setupAutoComplete_zsh d >/dev/null 2>&1
-    # For bash, test if the compgen/complete commands exists
-    elif type compgen >/dev/null 2>&1
-    then
-        _d_setupAutoComplete_bash
-        complete -F _d_setupAutoComplete_bash d >/dev/null 2>&1
-    fi
-}
-
-_d_setupAutoComplete
+    complete -F _d_setupAutoComplete_bash d >/dev/null 2>&1
+fi
